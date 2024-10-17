20909077ec581d7a9327a3c82f72625c6969b279
milosmekota <milos.mekota@post.cz>
2024-10-17 18:49:34 +0200

    Ready to deploy

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://root:root@bookstore.7q0fa.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore";
---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5555",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+app.get("/", (request, response) => {
+  return response.status(234);
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
